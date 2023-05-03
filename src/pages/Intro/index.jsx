0c354f4f872da23b49ab1acb4cb2f341b2d9b1be
MisterCxmpy <cosgunhussein@gmail.com>
2023-05-03 16:36:46 +0100

    fix container bug on intro page

--- src/pages/Intro/index.jsx
+++ src/pages/Intro/index.jsx
@@ -1,10 +1,12 @@
-import { useEffect, useRef } from "react";
+import { useEffect, useRef, useState } from "react";
 import styles from "./index.module.css";
 
 export default function Intro() {
 
-  const intro1Ref = useRef()
-  const intro2Ref = useRef()
+  const [load, setLoad] = useState(false)
+
+  const intro1Ref = useRef();
+  const intro2Ref = useRef();
   const message1Ref = useRef();
   const message2Ref = useRef();
   const continueBtnRef = useRef();
@@ -19,14 +21,14 @@ export default function Intro() {
     "Take a mental health day once per month.",
     "Learn a relaxation technique.",
     "Identify and challenge negative thoughts.",
-    "Celebrate small victories and progress."
+    "Celebrate small victories and progress.",
   ];
 
   useEffect(() => {
     setTimeout(() => {
       intro1Ref.current.style.opacity = 1;
     }, 500);
-    
+
     setTimeout(() => {
       message1Ref.current.style.opacity = 1;
     }, 2000);
@@ -41,9 +43,10 @@ export default function Intro() {
   }, []);
 
   function switchInfo() {
-    intro1Ref.current.style.opacity = 0
+    intro1Ref.current.style.opacity = 0;
     setTimeout(() => {
-      intro1Ref.current.remove()
+      intro1Ref.current.remove();
+      setLoad(true)
     }, 1000);
 
     setTimeout(() => {
@@ -57,50 +60,64 @@ export default function Intro() {
         <div className={styles["heading"]}>
           <h1>
             Hello! Welcome to &#8203;
-            <button className={styles["bounce"]} style={{ color: '#9A9FDD' }}>M</button>
-            <button className={styles["bounce"]} style={{ color: '#9A9FDD' }}>H</button>
-            <button className={styles["bounce"]} style={{ color: '#9A9FDD' }}>M</button>
-            <button className={styles["bounce"]} style={{ color: '#9A9FDD' }}>!</button>
+            <button className={styles["bounce"]} style={{ color: "#9A9FDD" }}>
+              M
+            </button>
+            <button className={styles["bounce"]} style={{ color: "#9A9FDD" }}>
+              H
+            </button>
+            <button className={styles["bounce"]} style={{ color: "#9A9FDD" }}>
+              M
+            </button>
+            <button className={styles["bounce"]} style={{ color: "#9A9FDD" }}>
+              !
+            </button>
           </h1>
           <h3>Explain the goal of the app</h3>
         </div>
         <p ref={message1Ref} className={styles["message"]}>
           Lorem ipsum dolor sit amet consectetur adipisicing elit. Illum quasi
           necessitatibus labore sapiente minus debitis maxime libero id quo
-          inventore, porro ipsa aspernatur unde modi quaerat laboriosam, deserunt
-          molestiae. Explicabo?
+          inventore, porro ipsa aspernatur unde modi quaerat laboriosam,
+          deserunt molestiae. Explicabo?
         </p>
         <p ref={message2Ref} className={styles["message"]}>
           Lorem ipsum dolor sit amet consectetur adipisicing elit. Illum quasi
           necessitatibus labore sapiente minus debitis maxime libero id quo
-          inventore, porro ipsa aspernatur unde modi quaerat laboriosam, deserunt
-          molestiae. Explicabo?
+          inventore, porro ipsa aspernatur unde modi quaerat laboriosam,
+          deserunt molestiae. Explicabo?
         </p>
-        <button ref={continueBtnRef} className={`${styles["continue-btn"]} btn`} onClick={switchInfo}>Continue</button>
+        <button
+          ref={continueBtnRef}
+          className={`${styles["continue-btn"]} btn`}
+          onClick={switchInfo}
+        >
+          Continue
+        </button>
       </div>
-      <div ref={intro2Ref} className={styles["container"]}>
-        <div className={styles["heading"]}>
-          <h1>
-            What are you looking to achieve?
-          </h1>
-          <h3>Select up to 5 options</h3>
+      {load ? (
+        <div ref={intro2Ref} className={styles["container"]}>
+          <div className={styles["heading"]}>
+            <h1>What are you looking to achieve?</h1>
+            <h3>Select up to 5 options</h3>
+          </div>
+          <div className={styles["selection-list"]}>
+            {mentalHealthGoals.map((goal, i) => {
+              return <Selection count={i} message={goal} />;
+            })}
+          </div>
         </div>
-        <div className={styles["selection-list"]}>
-          {mentalHealthGoals.map((goal, i) => {
-            return(
-              <Selection count={i} message={goal}/>
-            )
-          })}
-        </div>
-      </div>
+      ) : (
+        ""
+      )}
     </>
   );
 }
 
-function Selection({count, message}) {
+function Selection({ count, message }) {
   return (
     <div className={`${styles["selection"]}`} key={count}>
       <h4>{message}</h4>
     </div>
-  )
+  );
 }
