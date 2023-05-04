0a3e6161dc0e1cd1e8439a328f2eacbf9cb7d068
sord-dev <75338985+sord-dev@users.noreply.github.com>
2023-05-04 12:13:13 +0100

    adding ai mentor fucntionality

--- src/pages/AIMentor/index.jsx
+++ src/pages/AIMentor/index.jsx
@@ -1,14 +1,34 @@
+/* eslint-disable react/prop-types */
+import { useState } from "react";
 import styles from "./index.module.css"
 
 export default function AIMentor() {
+  const [history, setHistory] = useState([]);
+  const [input, setInput] = useState();
+
+  const handleSendMessage = async e => {
+    if(!input) return;
+    e.preventDefault()
+    handleSendUserMessage(e)
+    let options = { method: 'POST', headers: { 'Content-Type': 'application/json' }, body: JSON.stringify({message: input}) };
+    let res = await fetch('http://localhost:3000/mentor/chat', options)
+
+    let response = await res.json();
+    setHistory(prev => [...prev, {id: Math.floor(Math.random() * 7863), isYou: false, message: response.message}]);
+  } 
+
+  const handleSendUserMessage = async e => {
+    e.target.reset()
+    setHistory(prev => [...prev, {id: Math.floor(Math.random() * 7863), isYou: true, message: input}]);
+  } 
+
   return (
     <div className="layout">
       <div className={styles["container"]}>
         <div className={styles["messages-container"]}>
-          <Message isYou={true} message={"Hello, I am not doing too good day :("}/>
-          <Message isYou={false} message={"Don't worry pal, I am here to help!"}/>
+        {history.map(m => <Message key={m.id} isYou={m.isYou} message={m.message}/>)}
         </div>
-        <div className={styles["input-box"]}>
+        <div className={styles["input-box"]} onSubmit={handleSendMessage}>
           <div className={styles["options"]}></div>
           <form className={styles["input-form"]}>
             <div className={styles["input"]}>
@@ -17,6 +37,7 @@ export default function AIMentor() {
                 type="text"
                 placeholder="Enter your message here"
                 required
+                onChange={(e) => setInput(e.target.value)}
               />
               <button className={`${styles["submit"]} btn`}>Send Message</button>
             </div>
@@ -27,7 +48,7 @@ export default function AIMentor() {
   )
 }
 
-function Message({isYou, message}) {
+function Message({ isYou, message }) {
   return (
     <div className={`${styles[isYou ? "user-message" : "ai-message"]} ${styles["message"]}`}>
       <div className={styles["profile-picture"]}></div>
