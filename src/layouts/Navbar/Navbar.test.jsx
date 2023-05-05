c81d67d38c5c6f6820710755b352425c9301c50f
sord-dev <75338985+sord-dev@users.noreply.github.com>
2023-05-05 11:16:11 +0100

    here we go

--- src/layouts/Navbar/Navbar.test.jsx
+++ src/layouts/Navbar/Navbar.test.jsx
@@ -1,30 +1,37 @@
-import React from 'react';
-import { describe, expect, beforeEach, afterEach, it } from 'vitest';
-import { screen, render, cleanup } from '@testing-library/react';
-import { BrowserRouter } from 'react-router-dom';
- 
-import matchers from '@testing-library/jest-dom/matchers'
-expect.extend(matchers)
+import React from "react";
+import { describe, expect, beforeEach, afterEach, it, vi } from "vitest";
+import { screen, render, cleanup } from "@testing-library/react";
+import { BrowserRouter } from "react-router-dom";
 
-import Navbar from '.';
+import matchers from "@testing-library/jest-dom/matchers";
+expect.extend(matchers);
 
-describe('Navbar Component', () => {
-    beforeEach(() => {
-        render(
-          <BrowserRouter>
-              <Navbar />
-          </BrowserRouter>
-        )
-    })
-    
-    afterEach(() => {
-        cleanup();
-    })
+import Navbar from ".";
+import { AuthProvider, AuthContext } from "../../contexts/authContext";
 
-    it("Displays a nav bar with 5 links", () => {
-        const nav = screen.getByRole('navigation-btns')
-        expect(nav).toBeInTheDocument();
-        expect(nav.childNodes.length).toBe(7)
-    })
-    
-})
+describe("Navbar Component", () => {
+  beforeEach(() => {
+    vi.spyOn(AuthContext, "useContext").mockReturnValue({
+      currentUser: { username: "banana" },
+    });
+
+    render(
+      <BrowserRouter>
+        <AuthProvider>
+          <Navbar />
+        </AuthProvider>
+      </BrowserRouter>
+    );
+  });
+
+  afterEach(() => {
+    cleanup();
+    jest.restoreAllMocks();
+  });
+
+  it("Displays a nav bar with 5 links", () => {
+    const nav = screen.getByRole("navigation-btns");
+    expect(nav).toBeInTheDocument();
+    expect(nav.childNodes.length).toBe(7);
+  });
+});
--- src/pages/404/index.jsx
+++ src/pages/404/index.jsx
@@ -5,7 +5,7 @@ export default function ErrorPage() {
         <div className='layout' style={{ textAlign: 'center', display: 'flex', justifyContent: 'center', alignItems:'center', height: "100vh" }}>
             <div>
                 <h1>Oops, seems like you&apos;ve gone to a page that doesn&apos;t exist.</h1>
-                <p>That&apos;s okay, you can get back <Link to={'/'} >here.</Link></p>
+                <p>That&apos;s okay, you can get back <Link to={'/'} ><span style={{textDecoration: "underline"}}>here.</span></Link></p>
             </div>
         </div>
     )
