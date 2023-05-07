ec759e4213fbc925648f8f943dc58e068db41850
sord-dev <75338985+sord-dev@users.noreply.github.com>
2023-05-07 01:05:59 +0100

    post list of tasks on homepage

--- src/pages/Home/index.jsx
+++ src/pages/Home/index.jsx
@@ -1,20 +1,43 @@
 /* eslint-disable no-unused-vars */
 
-import { useState } from "react"
+import { useState, useEffect } from "react"
 import { MeditationList, MeditationModal, Modal, TaskContainer, UserStats } from "../../components"
 import styles from "./index.module.css"
 
 import meditationTypes from '../../../assets/meditations.json'
+import { useAuth } from "../../contexts/authContext"
 
 export default function Home() {
+  const { user } = useAuth()
   const [meditation, setMeditation] = useState(null);
   const [modal, setModal] = useState(false);
+  const [shortTermGoals, setShortTermGoals] = useState(false);
 
   const handleSelectMeditation = (thing) => {
     console.log(thing);
     setModal(true)
     setMeditation(thing);
   }
+
+  useEffect(() => {
+    const getUserGoals = async () => {
+      let options = { method: 'POST', headers: { 'Content-Type': 'application/json' }, body: JSON.stringify({ user_id: user.user_id }) }
+      let response = await fetch('http://localhost:3000/user/st/goals', options)
+
+      let data = await response.json()
+
+      if (response.ok) {
+        setShortTermGoals(data)
+      }
+
+      console.log(data)
+    }
+
+    if (user && !user?.goals.length) {
+      getUserGoals()
+    }
+
+  }, [])
 
   return (
     <>
@@ -25,7 +48,7 @@ export default function Home() {
 
           <MeditationList items={meditationTypes} setMeditation={handleSelectMeditation} />
 
-          <TaskContainer />
+          <TaskContainer goals={shortTermGoals} />
         </div>
       </div>
 
