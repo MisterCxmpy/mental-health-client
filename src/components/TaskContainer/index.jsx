90bdda5cd6711e0a252ce00132c5cb497c8c07a1
MisterCxmpy <cosgunhussein@gmail.com>
2023-05-07 11:59:20 +0100

    add collapse chat

--- src/components/TaskContainer/index.jsx
+++ src/components/TaskContainer/index.jsx
@@ -24,6 +24,7 @@ function PromptTask({ task = 'Lorem ipsum.' }) {
       <h3>
         {task}
       </h3>
+        <input className={styles["prompt-checkbox"]} type="checkbox"/>
     </div>
   );
 }
