2164435cede7220b39379baf8861feec9f35e69c
MisterCxmpy <cosgunhussein@gmail.com>
2023-05-09 21:48:09 +0100

    fixed intro stuff

--- src/components/MindStoreIntro/index.jsx
+++ src/components/MindStoreIntro/index.jsx
@@ -1,37 +1,27 @@
-import React, { useState } from 'react';
+import React, { useEffect, useState } from 'react';
 import { Steps } from 'intro.js-react';
 import 'intro.js/introjs.css';
 
 const MindStoreIntroComponent = ({ onExit }) => {
   const [stepsEnabled, setStepsEnabled] = useState(true);
-  const [initialStep, setInitialStep] = useState(0);
-
-  const handleNextStep = () => {
-    const nextStep = initialStep + 1;
-    setInitialStep(nextStep);
-
-    if (nextStep === steps.length) {
-      // All steps completed, disable further steps and exit the intro
-      setStepsEnabled(false);
-      if (onExit) {
-        onExit(); // Call the onExit callback if provided
-      }
-    }
-  };
+  const [loading, setLoading] = useState(false)
 
   const steps = [
     {
-      element: '#card-1', // Target the first item in the marketplace list
+      element: '#Morgan', // Target the first item in the marketplace list
       intro: 'This is the first item in the Mind Store.',
-      position: 'bottom',
-      action: handleNextStep,
+      position: 'bottom'
     },
     {
-      element: '#card-1 button', // Target the buy button of the first item
+      element: '#Morgan button', // Target the buy button of the first item
       intro: 'Click here to buy the item.',
-      position: 'bottom',
-      action: handleNextStep,
+      position: 'bottom'
     },
+    {
+      element: '#my-ai-mentor',
+      intro: 'This is the My AIMentor feature.',
+      position: 'bottom'
+    }
   ];
 
   const handleExitIntro = () => {
@@ -41,17 +31,21 @@ const MindStoreIntroComponent = ({ onExit }) => {
     }
   };
 
-  
+  useEffect(() => {
+    setTimeout(() => {
+      setLoading(true)
+    }, 500);
+  }, [])
 
   return (
     <div>
-      <Steps
+      {loading ? <Steps
         enabled={stepsEnabled}
         steps={steps}
-        initialStep={initialStep}
+        initialStep={0}
         onExit={handleExitIntro}
-        onChange={handleStepChange}
-      />
+        options={{ hideNext: false }}
+      /> : null}
     </div>
   );
 };
